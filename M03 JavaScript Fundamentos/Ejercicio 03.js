b18debca960b78432e890541cba123a040a9474f
JuanFranciscoFernandez <juanfranciscofernandezrrhh@gmail.com>
2023-05-22 20:12:40 -0300

    ejercicios modulo 3 22del05

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -1,4 +1,4 @@
-cd/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
@@ -6,31 +6,38 @@ function sonIguales(x, y) {
    // De lo contrario, retorna false.
    // Tu código:
    if (x === y) { return true }
-   else if (x)
+   else { return false }
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1 === str1) { return true }
+   else { return false }
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90) { return true }
+   else { return false }
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num > 50) { return true }
+   else { return false }
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num ==
 }
 
 function esImpar(num) {
